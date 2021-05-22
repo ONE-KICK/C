7334aa8c12e85260964b47d90aa21129fe6f4297
ONE-KICK <82807718+ONE-KICK@users.noreply.github.com>
2021-05-22 08:50:47 +0800

    Array operation

---
+++ 2021.5.22.①.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int init(int arr[]) {
+	for (int i = 0; i < 10; i++) {
+		arr[i] = 0;
+	}
+	return 0;
+}
+
+int print(int arr[]) {
+	for (int i = 0; i < 10; i++) {
+		printf("arr[%d]=%d\n", i, arr[i]);
+	}
+	return 0;
+}
+void reverse(int arr[10]) {
+	for (int i = 9; i >= 0; i--) {
+		printf("%d ", arr[i]);
+	}
+}
+int main() {
+	int arr[10];
+	init(arr);
+	print(arr);
+	reverse(arr);
+	return 0;
+}
