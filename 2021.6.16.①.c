e64a64646747e0e39a332df3cc326330d9ad8187
ONE-KICK <82807718+ONE-KICK@users.noreply.github.com>
2021-06-15 23:31:49 -0500

    杨氏矩阵

---
+++ 2021.6.16.①.c
@@ -0,0 +1,38 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int findnum(int arr[][3], int x, int y, int k)
+{
+	int i = 0;
+	int j = x - 1;
+	while (i <= y && j >= 0)
+	{
+		if (arr[i][j] < k)
+		{
+			i++;
+		}
+		if (arr[i][j] > k)
+		{
+			j--;
+		}
+		else
+		{
+			return 1;
+		}
+	}
+	return 0;
+}
+int main()
+{
+
+	int k = 0;
+	scanf("%d", &k);
+	int arr[3][3] = { {1,3,5},{4,5,7},{5,8,9} };
+	if (findnum(arr, 3, 3, k))
+	{
+		printf("找到了\n");
+	}
+	else
+	{
+		printf("找到了\n");
+	}
+}
