2a4d609b8c6a0580279303e8e5978b20607d7a4b
ONE-KICK <82807718+ONE-KICK@users.noreply.github.com>
2021-11-15 16:48:14 +0800

    冒泡排序

--- 2021.11.12.①.c
+++ 2021.11.12.①.c
@@ -1,18 +1,27 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
-
-int myStrlen(char* str) {
-	int count = 0;
-	while (*str != '\0') {
-		count++;
-		str++;
+#include<string.h>
+ 
+void bubbleSort(int arr[],int size) {
+	int bound = size-1;//bound边界,控制循环次数
+	int cur = 0;
+	for (bound = size-1; bound >= 0; bound--) {
+		for (cur = 0; cur < bound; cur++) {
+			if (arr[cur] > arr[cur+1]) {
+				int tmp = arr[cur];
+				arr[cur] = arr[cur + 1];
+				arr[cur + 1] = tmp;
+			}
+		}
 	}
-	return count;
 }
 
 int main() {
-	char str[] = "abcd";
-	printf("%d", myStrlen(str));
-
+	int arr[] = { 8,4,9,87,10,13,2,6,1 };
+	int size = sizeof(arr) / sizeof(arr[0]);
+	bubbleSort(arr,size);
+	for (int i = 0; i < size; i++) {
+		printf("%d ", arr[i]);
+	}
 }
